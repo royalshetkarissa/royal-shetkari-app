2c1f653692f11e6842e23ffc2970373455ef1d4c
Royal Shetkari User <user@royalshetkari.com>
2026-06-02 16:11:25 +0530

    chore(backend): set static dev OTP to 123456 for easier non-prod testing

--- backend/src/utils/generateOtp.js
+++ backend/src/utils/generateOtp.js
@@ -1,6 +1,9 @@
 const crypto = require('crypto');
 
 function generateOtp() {
+  if (process.env.NODE_ENV !== 'production') {
+    return '123456';
+  }
   return String(crypto.randomInt(100000, 1000000));
 }
 
